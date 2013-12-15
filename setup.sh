ff937cea3f974bc71edb3b5486dda147bf455c5d
Alex Blackie <thealexblackie@gmail.com>
2013-12-15 00:41:06 -0800

    Add basic setup script that probably sucks
    - Literally just `ln -s` a bunch of times because a proper Rakefile
      or something fun like that would be too much work. And really,
      this works so fuck everything else.

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir ~/.config
+ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.commonrc ~/.commonrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.fonts.conf ~/.fonts.conf
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.gemrc ~/.gemrc
+ln -s ~/.dotfiles/.ruby-version ~/.ruby-version
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/.config/terminator ~/.config/terminator
+ln -s ~/.dotfiles/.config/openbox ~/.config/openbox
+ln -s ~/.dotfiles/.config/tint2 ~/.config/tint2
+ln -s ~/.dotfiles/.config/deadbeef ~/.config/deadbeef
