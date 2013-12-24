6047b772b37408582d32581e40e80112d8007a51
Alex Blackie <thealexblackie@gmail.com>
2013-12-23 23:01:23 -0800

    Remove deadbeef from dotfiles
    - It doesn't work too well, and seems to change constantly. Plus
      I've mostly moved to cmus now, so I don't really need it.

--- setup.sh
+++ setup.sh
@@ -15,4 +15,4 @@ ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/.dotfiles/.config/terminator ~/.config/terminator
 ln -s ~/.dotfiles/.config/openbox ~/.config/openbox
 ln -s ~/.dotfiles/.config/tint2 ~/.config/tint2
-ln -s ~/.dotfiles/.config/deadbeef ~/.config/deadbeef
+ln -s ~/.dotfiles/.themes ~/.themes
