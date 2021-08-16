ab2238c536dd694f0c622b26bbac6d1d663a4eb9
Alex Blackie <alex@alexblackie.com>
2021-08-16 13:14:52 -0700

    Move non-plugin config into init.lua
    Splitting this into several files was really not worth it when there is
    probably <50 SLOC outside of plugin-related stuff.
    
    This leaves the separate files for custom functions and plugins, and
    puts the "general" configuration back into `init.lua` which makes it
    easier to find and simpler.

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -1,4 +1,68 @@
 require('plugins')
-require('general')
-require('keybindings')
-require('automation')
+
+--
+-- Editing
+--
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+
+--
+-- Interface and theming
+--
+vim.opt.termguicolors = true
+vim.opt.background = 'dark'
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.colorcolumn = { 80, 100 }
+vim.opt.winwidth = 107
+
+vim.g.tokyonight_style = 'night'
+
+vim.cmd 'colorscheme tokyonight'
+
+
+--
+-- Keybindings
+--
+
+-- Disable hotkey manpage lookups
+vim.api.nvim_set_keymap('n', 'K', '<NOP>', { noremap = true })
+
+-- Open file browser sidebar
+vim.api.nvim_set_keymap('n', '<leader>t', ':NERDTreeToggle<CR>', {})
+
+-- Find current file in file tree sidebar
+vim.api.nvim_set_keymap('n', '<leader>b', ':NERDTreeFind<CR>', {})
+
+-- Open fuzzy file finder
+vim.api.nvim_set_keymap('n', '<leader>f', ':Files<CR>', {})
+
+-- Trigger code completion
+vim.api.nvim_set_keymap('i', '<c-space>', 'coc#refresh()', { expr = true, silent = true })
+
+-- Code navigation: Go to definition
+vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { silent = true })
+
+-- Open popup terminal prefilled with the current filename
+vim.api.nvim_set_keymap('n', '<leader>e', ':call v:lua.ExecuteOnCurrentFile(expand(\'%\'))<CR>', {})
+
+
+--
+-- Automation
+--
+
+-- Highlight trailing whitespace
+vim.cmd 'autocmd BufWinEnter * match ErrorMsg /\\s\\+$/'
+vim.cmd 'autocmd InsertLeave * match ErrorMsg /\\s\\+$/'
+vim.cmd 'autocmd InsertEnter * match ErrorMsg /\\s\\+\\%#\\@<!$/'
+
+-- Trim trailing whitespace on save
+vim.cmd 'autocmd BufWritePre * :%s/\\s\\+$//e'
+
+-- exit if nerdtree is the only buffer left
+vim.cmd 'autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif'
+
+-- Highlight the hovered symbol and its references, if supported by the LS
+vim.cmd 'autocmd CursorHold * silent call CocActionAsync("highlight")'
