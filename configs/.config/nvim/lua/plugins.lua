764580e75a6f2686808f8d0f5fab8024938fe719
Alex Blackie <alex@blackie.me>
2022-09-22 10:02:46 -0700

    Prevent treesitter and vim-polyglot fighting
    Elixir, Lua, and a few other languages have had really annoying indent
    issues for a while and I finally tracked it down to vim-polyglot loading
    in addition to treesitter stuff, and I guess the two would fight for
    control and the result was a bunch of broken indentation and whatnot.

--- configs/.config/nvim/lua/plugins.lua
+++ configs/.config/nvim/lua/plugins.lua
@@ -23,28 +23,34 @@ require('lualine').setup({
 	extensions = { },
 })
 
+local treesitter_langs = {
+	'bash',
+	'c',
+	'css',
+	'dockerfile',
+	'elixir',
+	'erlang',
+	'go',
+	'gomod',
+	'graphql',
+	'hcl',
+	'heex',
+	'java',
+	'javascript',
+	'json',
+	'lua',
+	'python',
+	'ruby',
+	'rust',
+	'yaml',
+}
+
+-- Ensure treesitter and polyglot do not fight for control of the same
+-- languages. This causes all sorts of weird autoindent issues.
+vim.g.polyglot_disabled = treesitter_langs
+
 require('nvim-treesitter.configs').setup({
-	ensure_installed = {
-		'bash',
-		'c',
-		'css',
-		'dockerfile',
-		'elixir',
-		'erlang',
-		'go',
-		'gomod',
-		'graphql',
-		'hcl',
-		'heex',
-		'java',
-		'javascript',
-		'json',
-		'lua',
-		'python',
-		'ruby',
-		'rust',
-		'yaml',
-	},
+	ensure_installed = treesitter_langs,
 	ignore_install = { },
 	highlight = { enable = true },
 })
